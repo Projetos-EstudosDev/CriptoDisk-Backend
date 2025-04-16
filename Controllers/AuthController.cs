44f6389d3b1cb0c7187bdaee1bd2d71c963b4ca3
Patrick-Nahan <patrick.nahan7@gmail.com>
2025-04-16 14:21:21 -0300

    conexao com o banco de dados e a api de cadastro finalizada

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,67 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BackCriptoDisk2.Models;
+using BackCriptoDisk2.Data;
+
+
+namespace BackCriptoDisk2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public AuthController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<AuthController>
+        [HttpGet]
+        public ActionResult<List<Cadastro>> PegarCadastros()
+        {
+            var cadas = _context.usuarios.ToList();
+            return Ok(cadas);
+        }
+
+        // GET api/<AuthController>/5
+        [HttpGet("{id}")]
+        public ActionResult<Cadastro> PegarCadastroID(int id)
+        {
+            var buscar = _context.usuarios.Find(id);
+            if (buscar == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(buscar);
+        }
+
+        // POST api/<AuthController>
+        [HttpPost("Cadastro")]
+        public async Task<ActionResult<Cadastro>> CriarConta(Cadastro cadastro)
+        {
+            if (cadastro == null)
+            {
+                return BadRequest("Dados invalidos");
+            }
+
+            _context.usuarios.Add(cadastro);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(PegarCadastroID), new { id = cadastro.id }, cadastro);
+        }
+
+        // PUT api/<AuthController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<AuthController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
