58f2be8b3dce60cf962048f2d5eb37c8016f837f
Patrick-Nahan <patrick.nahan7@gmail.com>
2025-04-16 14:09:09 -0300

    Conexao com  o banco e api de cadastro finalizada

--- Models/Cadastro.cs
+++ Models/Cadastro.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BackCriptoDisk2.Models;
 
 public class Cadastro
 {
+    [Key]
     public int id { get; set; }
     public string? nome { get; set; }
     public string? username { get; set; }
